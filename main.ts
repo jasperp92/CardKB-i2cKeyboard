27bacc8ad6ac24a9788584077697ba81186a027e
Jasper <jasper.precht@web.de>
2022-09-01 11:52:40 +0200

    two functions
    noimplicitreturns = false

--- main.ts
+++ main.ts
@@ -1,10 +1,51 @@
-
 /**
  * Functions are mapped to blocks using various macros
  * in comments starting with %. The most important macro
  * is "block", and it specifies that a block should be
  * generated for an **exported** function.
  */
+
+enum characterFormat {
+    //% block=ASCII
+    ascii,
+    //% block=Charcode
+    charcode
+}
+
+//% color="#AA278D" weight=200 //% icon="\uf11c"
+namespace CardKB {
+    let i2cDevice = 95;
+
+    //% block
+    export function readCharCode() {
+        let message2: number = 0;
+        message2 = pins.i2cReadNumber(i2cDevice, NumberFormat.Int8LE, false)
+        if (message2 != 0) {
+            return message2
+        }
+        return;
+    }
+
+    //% block
+        export function readLetter() {
+        let message = 0;
+        message = pins.i2cReadNumber(i2cDevice, NumberFormat.Int8LE, false)
+        if (message != 0) {
+            return String.fromCharCode(message)
+        }
+        return;
+    }
+
+}
+
+
+
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+
 
 enum characterFormat {
     //% block=ASCII
@@ -32,3 +73,6 @@ namespace CardKB {
     }
 
 }
+
+
+ */
--- test.ts
+++ test.ts
@@ -11,3 +11,18 @@ basic.forever(function () {
         basic.showIcon(IconNames.ArrowWest)
     }
 })
+
+let letter = 0
+basic.forever(function () {
+    basic.showString(CardKB.readLetter())
+    letter = CardKB.readCharCode()
+    if (letter == -73) {
+        basic.showIcon(IconNames.ArrowEast)
+    } else if (letter == -74) {
+        basic.showIcon(IconNames.ArrowSouth)
+    } else if (letter == -75) {
+        basic.showIcon(IconNames.ArrowNorth)
+    } else if (letter == -76) {
+        basic.showIcon(IconNames.ArrowWest)
+    }
+})
